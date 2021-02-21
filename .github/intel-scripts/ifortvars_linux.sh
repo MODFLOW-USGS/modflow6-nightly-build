05f301f814be58fe236afa460d037b756a604bf2
Joseph D Hughes <jdhughes@usgs.gov>
2021-02-21 14:00:56 -0600

    ci(intel): add intel fortran compiler

--- .github/intel-scripts/ifortvars_linux.sh
+++ .github/intel-scripts/ifortvars_linux.sh
@@ -8,6 +8,9 @@
 LATEST_VERSION=$(ls -1 /opt/intel/oneapi/compiler/ | grep -v latest | sort | tail -1)
 # shellcheck source=/dev/null
 export ONEAPI_DIR="/opt/intel/oneapi/compiler/$LATEST_VERSION/env/"
+echo "$ONEAPI_DIR"
 echo "source $ONEAPI_DIR/vars.sh --install" >> "$HOME/.bash_profile"
 
-# source /opt/intel/oneapi/compiler/"$LATEST_VERSION"/env/vars.sh
+source /opt/intel/oneapi/compiler/"$LATEST_VERSION"/env/vars.sh
+
+which ifort
