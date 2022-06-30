399220ef645ecc4aff71cfe2d22f3a2cfe9a4f55
Joseph Hughes <jdhughes@usgs.gov>
2022-06-30 15:09:34 -0500

    ci: update ci to use updated modflow6 repo scripts
    Update OneAPI installation. Update GH Actions versions for
    pre-canned workflow tasks. Modified GH Actions to use standard
    python instead of conda python.

---
+++ .github/common/install-python-std.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install wheel
+pip install requests appdirs numpy matplotlib pytest pytest-xdist meson ninja
+pip install https://github.com/modflowpy/flopy/zipball/develop
+pip install https://github.com/modflowpy/pymake/zipball/master
