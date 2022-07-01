de494ddf8e5d45f4ec7b74fa5f9a43f82e34b9dc
Joseph Hughes <jdhughes@usgs.gov>
2022-06-30 22:39:12 -0500

    ci: update ci to use updated modflow6 repo scripts
    Update OneAPI installation. Update GH Actions versions for
    pre-canned workflow tasks. Modified GH Actions to use standard
    python instead of conda python.

--- .github/intel-scripts/build_windows.bat
+++ .github/intel-scripts/build_windows.bat
@@ -2,9 +2,6 @@ REM SPDX-FileCopyrightText: 2020 Intel Corporation
 REM
 REM SPDX-License-Identifier: MIT
 
-set LANGUAGE=%1
-set VS_VER=%2
-
 @call "C:\Program Files (x86)\Intel\oneAPI\setvars-vcvarsall.bat" %VS_VER%
 
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
